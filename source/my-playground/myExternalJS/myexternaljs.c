e3c1029c99aee5e2dc38f987343e0440685295a8
Francesc Elies <francesc.elies@mbbm-ast.com>
2024-01-12 13:53:39 +0100

    javascript returns are truncated

--- source/my-playground/myExternalJS/myexternaljs.c
+++ source/my-playground/myExternalJS/myexternaljs.c
@@ -2,11 +2,14 @@
 #include "ext_mess.h"
 #include "ext_obex.h" // required for new style Max object
 #include "ext_post.h"
+#include "ext_proto.h"
+#include <stdio.h>
 
 typedef struct _myexternaljs {
   t_object ob; // the object itself (must be first)
   double myattr;
   t_object *patcher;
+  t_symbol *patcher_name;
 } t_myexternaljs;
 
 void *new_routine(t_symbol *s, long argc, t_atom *argv);
@@ -45,43 +48,72 @@ void *new_routine(t_symbol *s, long argc, t_atom *argv) {
   }
   // NOTE: Needs Max 8.6 at least
   x->patcher = gensym("#P")->s_thing;
+  x->patcher_name = jpatcher_get_name(x->patcher);
   return (x);
 }
+char *get_long_string() {
+  size_t desired_length_bytes =
+      32 * 1024 - 1; // 32KB (-1 for null ternimator)  32767 chars
+  //
+  // size_t desired_length_bytes = 32 * 1024; // 💥BOOM, max silently truncates
 
+  // Allocate memory for the string
+  char *long_string =
+      (char *)malloc(desired_length_bytes + 1); // +1 for the null terminator
+
+  if (long_string == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return NULL;
+  }
+
+  // Fill the string with a specific character (e.g., 'a')
+  for (size_t i = 0; i < desired_length_bytes; i++) {
+    long_string[i] = 'a';
+  }
+  long_string[desired_length_bytes - 1] = 'X';
+
+  // Add the null terminator at the end
+  long_string[desired_length_bytes] = '\0';
+  printf("%s", long_string);
+
+  return long_string;
+}
 void print(t_myexternaljs *x, t_symbol *s, long ac, t_atom *av) {
   post("The value of myattr is: %f", x->myattr);
 }
 
+#define AC 3
+
 t_max_err doEvilThingsWith(t_myexternaljs *x, t_symbol *s, long ac, t_atom *av,
                            t_atom *rv) {
-  t_atom a[1];
+  t_atom a[AC];
   t_symbol *js_input;
 
   if (ac == 1) {
-    long atom_type_1 = atom_gettype(&av[1]);
-    if (atom_type_1 == A_SYM) {
-      js_input = atom_getsym(&av[1]);
+    long first_atom = atom_gettype(av);
+    if (first_atom == A_SYM) {
+      js_input = atom_getsym(av);
       post("js_input %s", js_input->s_name);
     } else {
-      error("expected A_OBJ but got %d", atom_type_1);
+      error("expected A_OBJ but got %d", first_atom);
     }
   } else {
     error("missing argument for method doEvilThingsWith()");
   }
 
-  //
-  // object_obex_lookup(x, gensym("#P"), &patcher);
+  // Test 1: return a long string
+  char *a_long_string = get_long_string();
 
-  post("patcher address, using p = gensym(\"#P\")->s_thing in constructor: %lx",
-       x->patcher);
-
-  long result = jpatcher_get_count(x->patcher);
+  // Test 2: get current patcher object count
+  long number_objects_in_patcher = jpatcher_get_count(x->patcher);
 
   // store the result in the a array.
-  atom_setlong(a, result);
+  atom_setsym(a, gensym(a_long_string));
+  atom_setsym(&a[1], x->patcher_name);
+  atom_setlong(&a[2], number_objects_in_patcher);
 
   // return the result to js
-  atom_setobj(rv, object_new(gensym("nobox"), gensym("atomarray"), 1, a));
+  atom_setobj(rv, object_new(gensym("nobox"), gensym("atomarray"), AC, a));
 
   return MAX_ERR_NONE;
 }
--- source/my-playground/myExternalJS/test_myexternaljs.js
+++ source/my-playground/myExternalJS/test_myexternaljs.js
@@ -1,5 +1,13 @@
 m = MyExternalJS();
 
-function bang() {
-    post("Number of elements in patcher " + m.doEvilThingsWith("parent"));
-}
+result = m.doEvilThingsWith("parent");
+longString = result[0];
+patcherName = result[1];
+numElementsPatcher = result[2];
+
+    post("\nLast element long string " + longString.length + " chars, last char" + longString[32766]);
+    post("\nPatcher name " + patcherName);
+    post("\nNumber of elements in patcher " + numElementsPatcher);
+
+
+
