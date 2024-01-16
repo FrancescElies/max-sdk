8acd7385cc42c309e667039d9d35dc217157999a
Francesc Elies <francesc.elies@mbbm-ast.com>
2024-01-16 08:17:57 +0100

    result is an array

--- source/my-playground/myExternalJS/test_myexternaljs.js
+++ source/my-playground/myExternalJS/test_myexternaljs.js
@@ -1,6 +1,6 @@
 m = MyExternalJS();
 
-result = m.doEvilThingsWith("parent");
+result = m.doEvilThingsWith("parent")[0];
 
 post("result: " + result.slice(0, 10));
 post("Returned str length " + result.length + "\n");
